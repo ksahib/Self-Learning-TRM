0f22e0a76ba0f55085f0a18e701b39b43e12e648
niloy <niloy.ashraf@northsouth.edu>
2025-12-18 18:32:50 +0600

    wewe

--- models/recursive_reasoning/metaTRM.py
+++ models/recursive_reasoning/metaTRM.py
@@ -205,7 +205,13 @@ class MetaTRMInner(nn.Module):
         self.h_head = CastedLinear(
             self.config.hidden_size,
             len(self.config.h_cycle_choices),
-            b…
+            bias=False,
+        )
+        self.l_head = CastedLinear(
+            self.config.hidden_size,
+            len(self.config.l_cycle_choices),
+            bias=False,
+        )
 
     def _input_embeddings(self, inputs: torch.Tensor, puzzle_identifiers: Optional[torch.Tensor]):
         embedding = self.embed_tokens(inputs.to(torch.int32))
